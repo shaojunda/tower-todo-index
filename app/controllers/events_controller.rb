c45263f7a029fafa30288ce0f5686a4f00ec9e2e
shaojunda <shaojunda@gmail.com>
2016-09-19 01:22:27 +0800

    implement index and get_more_event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,11 @@
 class EventsController < ApplicationController
+  def index
+    @events = Event.limit(50)
+    @current_page = 1
+  end
+
+  def get_more_event
+    @current_page = params[:current_page]
+    @events = Event.limit(10).offset(50 * current_page)
+  end
 end
