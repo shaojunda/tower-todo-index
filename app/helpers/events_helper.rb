90d4b52a0c1470984316207d93f1ffa175088da6
shaojunda <shaojunda@gmail.com>
2016-09-19 13:48:22 +0800

    add render_event_content

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,4 +2,15 @@ module EventsHelper
   def render_created_at(create_at)
     create_at.strftime("%Y-%m-%d %H:%M:%S")
   end
+
+  def render_event_content(event)
+    if event.eventable.class == Todo
+      event.eventable.title
+    elsif event.eventable.class == Team
+      event.eventable.name
+    elsif event.eventable.class == Project
+      event.eventable.name
+    end
+  end
+
 end
