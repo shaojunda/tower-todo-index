e9ed59c4dff096102e9aa374be14dfd0ad0ea7a3
shaojunda <shaojunda@gmail.com>
2016-09-20 18:58:48 +0800

    add get_current_page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,24 @@
 class EventsController < ApplicationController
+  before_action :get_current_page
+
   def index
     @events = Event.limit(50).order("created_at desc")
     @group_events = @events.group_by{|e| e.ownerable_type}
-    @current_page = 1
   end
 
   def get_more_event
-    @current_page = params[:current_page]
     @events = Event.limit(10).offset(50 * current_page)
     @group_events = @events.group_by{|e| e.ownerable_type}
   end
+
+  protected
+
+  def get_current_page
+    @current_page = params[:current_page]
+    if @current_page.blank?
+      @current_page = 1
+    end
+  end
+
+
 end
